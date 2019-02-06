f664db46e58f9688efe59397b219f316308c0031
Aldo Fregoso <aldomx@gmail.com>
2019-02-06 16:39:00 -0600

    Added `pixi.js`.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {AppConsumer, Container, Stage} from '@inlet/react-pixi';
+import React, {Component} from 'react';
+
+import RotatingBunny from './components/rotating-bunny';
 
 class App extends Component {
+  state = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize =
+      (event) => {
+        if (this.onResizeTimeout != null) {
+          clearTimeout(this.onResizeTimeout);
+        }
+        this.onResizeTimeout = setTimeout(() => {
+          this.updateBounds(event.target.innerWidth, event.target.innerHeight);
+          this.onResizeTimeout = null;
+        }, 250);
+      }
+
+  updateBounds =
+      (width, height) => {
+        this.setState(() => ({width, height}));
+      }
+
   render() {
+    const {width, height} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Stage width={width} height={height}>
+        <Container x={width / 2} y={height / 2}>
+          <AppConsumer>
+            {app => <RotatingBunny app={app}/>}
+          </AppConsumer>
+        </Container>
+      </Stage>
     );
   }
 }
