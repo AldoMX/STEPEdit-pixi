1309103408b4540eead8c81988faccafe1bd79e6
Aldo Fregoso <aldomx@gmail.com>
2019-02-09 12:23:52 -0600

    Wait until the web fonts have been loaded to render the app.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+WebFont.load({
+  google: {
+    families: ['Open Sans'],
+  },
+  active: renderApp,
+  inactive: renderApp,
+});
